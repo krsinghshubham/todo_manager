0397df7ee6b95b95dcba0d483d284f5f178cbaa7
shubham <shubhamk604@gmail.com>
2021-03-16 21:03:10 +0530

    rendered list of todos from database into url (/todos)

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
     def index   #now we are defining what index does... so it reders a plain text as following. 
-        render plain: "Hello, this /todos! and the current time is \n #{DateTime.now.to_s(:short)}"
+        render plain: Todo.all.to_ary
     end
 end
