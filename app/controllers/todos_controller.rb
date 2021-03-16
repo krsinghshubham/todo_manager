401f9ad9f1eaacaabbb13aa6f6a597689216fc31
shubham <shubhamk604@gmail.com>
2021-03-16 22:42:46 +0530

    Got output in console, to get a modified output in url, we redefined what function todo_controller should render, and in todo.rb we defined that function( cleared the clutter.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
     def index   #now we are defining what index does... so it reders a plain text as following. 
-        render plain: Todo.all.to_ary
+        render plain: Todo.order(:due_date).map {|object| object.to_pleasant_string}.join("\n")
     end
 end
