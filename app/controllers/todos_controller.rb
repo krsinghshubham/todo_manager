8acff1c2844d38935e05032fc7637d990eeb30da
shubham <shubhamk604@gmail.com>
2021-03-24 18:36:52 +0530

    can see a single todo in /todo/:id/

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,14 @@ class TodosController < ApplicationController
     def index   #now we are defining what index does... so it reders a plain text as following. 
         render plain: Todo.order(:due_date).map {|object| object.to_pleasant_string}.join("\n")
     end
+
+    def show
+        id= params[:id]
+        todo= Todo.find(id)
+        render plain: todo.to_pleasant_string
+    end
 end
+
+
+# Params = As you might have guessed, params is an alias for the parameters method. params comes from ActionController::Base, which is accessed by your application via ApplicationController. Specifically, params refers to the parameters being passed to the controller via a GET or POST request.
+# In a GET request, params get passed to the controller from the URL in the user’s browser. For example, if our app’s controller looked like  def show @person = Person.find(params[:name]) endand the user typed in: http://example.com/person/avi then the controller would pass in {:name => “avi”} to the show method, which would set the @person instance variable to the person in the database with the name “avi”.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "todos", to: "todos#index" #for get-requst todo, route it to todos(defined by todos_controller, there define what index does)
+  get "todos/:id", to: "todos#show" #will define how the show will work in todos_controller
 end
