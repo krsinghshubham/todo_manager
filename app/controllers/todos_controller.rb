724d9d5c4cf850ba926e2d093d51f4f7b075db06
shubham <shubhamk604@gmail.com>
2021-03-27 21:18:34 +0530

    addedd a form so that a todo can be saved form the website.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,40 +1,34 @@
-class TodosController < ApplicationController   #project controller class, mostly inspired from projectName. 
-    def index   
-        # render plain: Todo.order(:due_date).map {|object| object.to_pleasant_string}.join("\n") #order the output by due_date
-        # we want index to render index.html.erb sitting in views folder and not directly from the data base using Todo object. 
-        #for this we have to create a file as "index.html" in views folder insider a user created sub folder todos (because we are going with TodosController)
-        render "index"
-    end
+class TodosController < ApplicationController #project controller class, mostly inspired from projectName.
 
-    def show
-        id= params[:id] # beacuase parameters cannot be passed from routes.
-        todo= Todo.find(id)
-        render plain: todo.to_pleasant_string
-    end
+  def index
+    # render plain: Todo.order(:due_date).map {|object| object.to_pleasant_string}.join("\n") #order the output by due_date
+    # we want index to render index.html.erb sitting in views folder and not directly from the data base using Todo object.
+    #for this we have to create a file as "index.html" in views folder insider a user created sub folder todos (because we are going with TodosController)
+    render "index"
+  end
 
-    def new
-    end
+  def show
+    id = params[:id] # beacuase parameters cannot be passed from routes.
+    todo = Todo.find(id)
+    render plain: todo.to_pleasant_string
+  end
 
-    def edit
-    end
-
-    def create
-        todo_text = params[:todo_text]
-        
-    end
-
-    def update
-    end
-
-    def destroy
-    end
-
+  def create
+    todo_text = params[:todo_text]
+    due_date = DateTime.parse(params[:due_date])
+    new_todo = Todo.create!(
+      todo_text: todo_text,
+      due_date: due_date,
+      completed: false,
+    )
+    # response_text = "Hey , your new todo created with id= #{new_todo.id}"
+    # render plain: response_text
+    redirect_to todos_path
+  end
 end
 
-
 # Params = As you might have guessed, params is an alias for the parameters method. params comes from ActionController::Base, which is accessed by your application via ApplicationController. Specifically, params refers to the parameters being passed to the controller via a GET or POST request.
-# In a GET request, params get passed to the controller from the URL in the user’s browser. For example, if our app’s controller looked like 
-#  def show 
-#    @person = Person.find(params[:name]) 
+# In a GET request, params get passed to the controller from the URL in the user’s browser. For example, if our app’s controller looked like
+#  def show
+#    @person = Person.find(params[:name])
 #     endand the user typed in: http://example.com/person/avi then the controller would pass in {:name => “avi”} to the show method, which would set the @person instance variable to the person in the database with the name “avi”.
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get "/", to: "todos#index"
 
-  resources :todos #using resources we need not to define the indivisual routes as we did below.
-  get "todos", to: "todos#index" #for get_type_request <url_name>, route it to todos(defined by todos_controller, as naming convention), there define what index does)
+  resources :todos 
+  
 end
+# get "todos", to: "todos#index" #for get_type_request <url_name>, route it to todos(defined by todos_controller, as naming convention), there define what index does)
 # routes first check in public folder if todos.html available or not. If not it will go to contoller(here todos_controller) and there find the method .
