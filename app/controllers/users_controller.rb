57dbe8b9a3a05c11433d242def64b9ca4b0a9ea3
shubham <shubhamk604@gmail.com>
2021-03-30 12:38:52 +0530

    blank

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
   end
 
   def create
-    user=User.create!(
+    User.create!(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
     )
-    session[:current_user_id] = user.id
-    redirect_to "/"
+    
+    redirect_to todos_path
   end
 end
